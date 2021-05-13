8cf738c9cb0eb08d7c2549220d74bdd3197d2db7
scrappy9 <50796594+scrappy9@users.noreply.github.com>
2021-05-13 11:02:14 +0200

    test(GT4500): updating tests using mochito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -7,34 +7,52 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
-
+  // class to be tested
   private GT4500 ship;
 
+  // dependencies (will be mocked)
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
+
   @BeforeEach
-  public void init(){
-    this.ship = new GT4500();
+  public void init(){    
+    primary = mock(TorpedoStore.class);
+    secondary = mock(TorpedoStore.class);
+
+    // object to be tested
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.isEmpty()).thenReturn(false);
+    when(primary.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).fire(anyInt());
+    verify(secondary, times(0)).fire(anyInt());
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.isEmpty()).thenReturn(false);
+    when(primary.fire(anyInt())).thenReturn(true);
+    when(secondary.isEmpty()).thenReturn(false);
+    when(secondary.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).fire(anyInt());
+    verify(secondary, times(0)).fire(anyInt());
   }
 
 }
